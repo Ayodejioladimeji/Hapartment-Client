fd745e426b29a5072d05e238a0d10a76e25cc8a4
layobright <layobright11.com>
2023-02-04 09:37:01 -0800

    added major updates

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,11 @@
 import Layout from "@/components/layout";
 import "@/styles/main.scss";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <script
+      <Script
         src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
         crossorigin="anonymous"
