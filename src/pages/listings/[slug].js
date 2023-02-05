aca8f0dbd077e4716cacd2ce5a81090acb3ffaa5
Ayodejioladimeji <layobright11@gmail.com>
2023-02-05 16:37:14 +0100

    adding carousel to the details page

--- src/pages/listings/[slug].js
+++ src/pages/listings/[slug].js
@@ -49,30 +49,6 @@ const ListingDetails = () => {
                 className="carousel slide"
                 data-bs-ride="carousel"
               >
-                {/* carousel indicators */}
-                <div className="carousel-indicators">
-                  <button
-                    type="button"
-                    data-bs-target="#carouselExampleIndicators"
-                    data-bs-slide-to="0"
-                    className="active"
-                    aria-current="true"
-                    aria-label="Slide 1"
-                  ></button>
-                  <button
-                    type="button"
-                    data-bs-target="#carouselExampleIndicators"
-                    data-bs-slide-to="1"
-                    aria-label="Slide 2"
-                  ></button>
-                  <button
-                    type="button"
-                    data-bs-target="#carouselExampleIndicators"
-                    data-bs-slide-to="2"
-                    aria-label="Slide 3"
-                  ></button>
-                </div>
-
                 <div className="carousel-inner">
                   <div className="carousel-item active">
                     <Image src={banner1} className="carousel-image" alt="..." />
