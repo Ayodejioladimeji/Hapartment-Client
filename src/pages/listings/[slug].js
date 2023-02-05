f63bf6f5ba2e580d90d8a07be39d799381748a7f
layobright <layobright11.com>
2023-02-05 05:02:15 -0800

    completed details image carousel

--- src/pages/listings/[slug].js
+++ src/pages/listings/[slug].js
@@ -62,17 +62,14 @@ const ListingDetails = () => {
                 </div>
 
                 {/* next and previous buttons */}
+
                 <button
                   className="carousel-control-prev"
                   type="button"
                   data-bs-target="#carouselExampleIndicators"
                   data-bs-slide="prev"
                 >
-                  <span
-                    className="carousel-control-prev-icon"
-                    aria-hidden="true"
-                  ></span>
-                  <span className="visually-hidden">Previous</span>
+                  <i className="bi control bi-arrow-left-circle-fill"></i>
                 </button>
 
                 <button
@@ -81,11 +78,7 @@ const ListingDetails = () => {
                   data-bs-target="#carouselExampleIndicators"
                   data-bs-slide="next"
                 >
-                  <span
-                    className="carousel-control-next-icon"
-                    aria-hidden="true"
-                  ></span>
-                  <span className="visually-hidden">Next</span>
+                  <i className="bi control bi-arrow-right-circle-fill"></i>
                 </button>
               </div>
             </div>
