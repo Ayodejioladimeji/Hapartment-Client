d2d386bc1ac3219fe11b6c398874482b055e52e9
layobright <layobright11.com>
2023-02-13 11:12:16 -0800

    added meta tags

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,6 +5,7 @@ import Script from "next/script";
 import Aos from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
   // THE SECTION OF THE AOS
@@ -14,37 +15,58 @@ export default function App({ Component, pageProps }) {
 
   return (
     <>
-      <meta name="description" content="Easy way to find a perfect home" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <Head>
+        {/* <meta
+          name="google-site-verification"
+          content="vN0oQ_7GfBdOye17B6gsByAeSRHS1UE47iqXKj0d9ak"
+        /> */}
+        <meta
+          name="description"
+          content="Hapartment is bringing agents with renters together by providing them with the simplest and most cost-effective route to renting properties online"
+        />
+        <meta
+          name="keywords"
+          content="Hapartment, hapartments, Hapartments, hapartment, real estate, agents, landlord, tenant, renting website, apartment, rent house, leasing house "
+        />
+
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <meta name="title" content="Hapartment - Homepage" />
+        <meta
+          property="og:title"
+          content="We help users search for apartment easily and provide them with adequate information to help them make well-informed decisions."
+        />
+        <meta
+          property="og:description"
+          content="Hapartment is bringing agents with renters together by providing them with the simplest and most cost-effective route to renting properties online"
+        />
+
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/devsource/image/upload/v1671309144/hapartment/logo512_otvwxg.png"
+        />
+        <meta property="og:type" content="website" />
+
+        <meta property="og:url" content="https://hapartment.org/" />
+        <meta name="twitter:card" content="summary_large_image" />
+
+        <meta name="robots" content="index, nofollow" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+        <meta name="author" content="Hapartment Digital Marketplace" />
+
+        <meta
+          property="og:site_name"
+          content="Hapartment Digital Marketplace"
+        />
+        <meta name="twitter:image:alt" content="Hapartment" />
+      </Head>
+
       <Script
         src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
         crossOrigin="anonymous"
       />
-      <meta name="title" content="Hapartment - Homepage" />
-      {/* <meta
-        name="keywords"
-        content="Hapartment apartment agent tenant rent renters renthouse landlord lease property listings"
-      />
-      <meta name="robots" content="index, nofollow" />
-      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-      <meta name="language" content="English" />
-      <meta name="author" content="Hapartment Digital Marketplace" /> */}
-
-      {/* Essential META Tags */}
-      {/* <meta property="og:title" content="Hapartment Homepage" />
-      <meta
-        property="og:description"
-        content="Easy way to find a perfect home"
-      />
-      <meta property="og:image" content="" />
-      <meta property="og:url" content="" />
-      <meta name="twitter:card" content="summary_large_image" /> */}
-
-      {/* Non-Essential, But Recommended  */}
-      {/* <meta property="og:site_name" content="Hapartment" />
-      <meta name="twitter:image:alt" content="Hapartment" /> */}
-
       <DataProvider>
         <Layout>
           <Component {...pageProps} />
--- src/pages/listings/[slug].js
+++ src/pages/listings/[slug].js
@@ -10,6 +10,7 @@ import { useRouter } from "next/router";
 import Slider from "@/common/Slider";
 import { getDataApis } from "@/utils/fetchData";
 import Map from "./../../utils/map";
+import Head from "next/head";
 
 //
 
@@ -28,138 +29,188 @@ const ListingDetails = (props) => {
   } = props.listing;
 
   return (
-    <section className="white">
-      <div className="container">
-        <div className="row">
-          <div className="listing-details col-lg-9 mt-5">
-            <div className="back mb-4">
-              <button className="back-button" onClick={() => router.back()}>
-                <i className="bi bi-chevron-left"></i> Go back
-              </button>
-            </div>
+    <>
+      <Head>
+        <title>Listing Details</title>
+        <meta
+          name="description"
+          content="Hapartment is bringing agents with renters together by providing them with the simplest and most cost-effective route to renting properties online"
+        />
+        <meta
+          name="keywords"
+          content="Hapartment, hapartments, Hapartments, hapartment, real estate, agents, landlord, tenant, renting website, apartment, rent house, leasing house "
+        />
 
-            <div className="row">
-              <div className="col-md-8">
-                <h3>{property_type}</h3>
-                <div className="address">
-                  <i className="bi bi-geo-alt-fill"></i>
-                  {address}
-                </div>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <meta name="title" content="Hapartment - Homepage" />
+        <meta
+          property="og:title"
+          content="We help users search for apartment easily and provide them with adequate information to help them make well-informed decisions."
+        />
+        <meta
+          property="og:description"
+          content="Hapartment is bringing agents with renters together by providing them with the simplest and most cost-effective route to renting properties online"
+        />
+
+        <meta
+          property="og:image"
+          content="https://res.cloudinary.com/devsource/image/upload/v1671309144/hapartment/logo512_otvwxg.png"
+        />
+        <meta property="og:type" content="website" />
+
+        <meta property="og:url" content="https://hapartment.org/" />
+        <meta name="twitter:card" content="summary_large_image" />
+
+        <meta name="robots" content="index, nofollow" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="language" content="English" />
+        <meta name="author" content="Hapartment Digital Marketplace" />
+
+        <meta
+          property="og:site_name"
+          content="Hapartment Digital Marketplace"
+        />
+        <meta name="twitter:image:alt" content="Hapartment" />
+      </Head>
+
+      <section className="white">
+        <div className="container">
+          <div className="row">
+            <div className="listing-details col-lg-9 mt-5">
+              <div className="back mb-4">
+                <button className="back-button" onClick={() => router.back()}>
+                  <i className="bi bi-chevron-left"></i> Go back
+                </button>
               </div>
 
-              <div className="col-md-4">
-                <div className="price float-md-end float-sm-start">
-                  ₦ {price}
-                </div>
-              </div>
-            </div>
-
-            <div className="carousel-container my-4">
-              <Slider images={images} />
-            </div>
-
-            {/* details */}
-            <div className="details">
-              <div className="details-box">
-                <FaBed className="details-icons" />
-                {bedrooms}{" "}
-                {bedrooms === "1"
-                  ? "Bedroom"
-                  : bedrooms === "2" ||
-                    bedrooms === "3" ||
-                    bedrooms === "4" ||
-                    bedrooms === "5+"
-                  ? "Bedrooms"
-                  : ""}
-              </div>
-              <div className="details-box">
-                <FaBath className="details-icons" />
-                {bathrooms} Bathrooms
-              </div>
-              <div className="details-box">
-                <FaToilet className="details-icons" />
-                {toilets} Toilets
-              </div>
-            </div>
-
-            <div className="details-tab mt-5">
-              <div className="tab-section">
-                <div className="deliver-box">
-                  <ul className="nav nav-tabs">
-                    <li className="nav-item">
-                      <a
-                        className="nav-link active"
-                        data-bs-toggle="tab"
-                        href="#description"
-                      >
-                        Description
-                      </a>
-                    </li>
-                    <li className="nav-item">
-                      <a className="nav-link" data-bs-toggle="tab" href="#map">
-                        View map
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-
-                <div className="tab-content">
-                  <div className="tab-pane active" id="description">
-                    <div className="package-box mt-4 px-4 py-3">
-                      <Description item={props.listing} />
-                    </div>
+              <div className="row">
+                <div className="col-md-8">
+                  <h3>{property_type}</h3>
+                  <div className="address">
+                    <i className="bi bi-geo-alt-fill"></i>
+                    {address}
                   </div>
+                </div>
 
-                  <div className="tab-pane" id="map">
-                    <div className="package-box mt-4 py-3">
-                      <Map lat={map[0].latitude} lng={map[0].longitude} />
-                    </div>
+                <div className="col-md-4">
+                  <div className="price float-md-end float-sm-start">
+                    ₦ {price}
                   </div>
                 </div>
               </div>
+
+              <div className="carousel-container my-4">
+                <Slider images={images} />
+              </div>
+
+              {/* details */}
+              <div className="details">
+                <div className="details-box">
+                  <FaBed className="details-icons" />
+                  {bedrooms}{" "}
+                  {bedrooms === "1"
+                    ? "Bedroom"
+                    : bedrooms === "2" ||
+                      bedrooms === "3" ||
+                      bedrooms === "4" ||
+                      bedrooms === "5+"
+                    ? "Bedrooms"
+                    : ""}
+                </div>
+                <div className="details-box">
+                  <FaBath className="details-icons" />
+                  {bathrooms} Bathrooms
+                </div>
+                <div className="details-box">
+                  <FaToilet className="details-icons" />
+                  {toilets} Toilets
+                </div>
+              </div>
+
+              <div className="details-tab mt-5">
+                <div className="tab-section">
+                  <div className="deliver-box">
+                    <ul className="nav nav-tabs">
+                      <li className="nav-item">
+                        <a
+                          className="nav-link active"
+                          data-bs-toggle="tab"
+                          href="#description"
+                        >
+                          Description
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a
+                          className="nav-link"
+                          data-bs-toggle="tab"
+                          href="#map"
+                        >
+                          View map
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+
+                  <div className="tab-content">
+                    <div className="tab-pane active" id="description">
+                      <div className="package-box mt-4 px-4 py-3">
+                        <Description item={props.listing} />
+                      </div>
+                    </div>
+
+                    <div className="tab-pane" id="map">
+                      <div className="package-box mt-4 py-3">
+                        <Map lat={map[0].latitude} lng={map[0].longitude} />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="disclaimer my-5">
+                <h6>Disclaimer</h6>
+                <p>
+                  Hapartment Digital Marketplace makes no warranty as to the
+                  accuracy or completeneness of the advertisement or any linked
+                  or associated information on the information displayed about
+                  this property, and Hapartment Digital Marketplace has no
+                  control over the content.
+                </p>
+                <p>
+                  Hapartment Digital Marketplace shall not in any way be held
+                  liable for the actions of any agent or landlord on or off this
+                  website.
+                </p>
+              </div>
             </div>
 
-            <div className="disclaimer my-5">
-              <h6>Disclaimer</h6>
-              <p>
-                Hapartment Digital Marketplace makes no warranty as to the
-                accuracy or completeneness of the advertisement or any linked or
-                associated information on the information displayed about this
-                property, and Hapartment Digital Marketplace has no control over
-                the content.
-              </p>
-              <p>
-                Hapartment Digital Marketplace shall not in any way be held
-                liable for the actions of any agent or landlord on or off this
-                website.
-              </p>
-            </div>
-          </div>
+            <div className="col-lg-3 mt-5">
+              <div className="advert-image-box mb-5">
+                <Image src={banner1} alt="" />
+              </div>
+              <div className="advert-image-box mb-5">
+                <Image src={banner5} alt="" />
+              </div>
+              <div className="advert-image-box mb-5">
+                <Image src={banner2} alt="" />
+              </div>
+              <div className="advert-image-box mb-5">
+                <Image src={banner3} alt="" />
+              </div>
+              <div className="advert-image-box mb-5">
+                <Image src={banner4} alt="" priority={true} />
+              </div>
 
-          <div className="col-lg-3 mt-5">
-            <div className="advert-image-box mb-5">
-              <Image src={banner1} alt="" />
-            </div>
-            <div className="advert-image-box mb-5">
-              <Image src={banner5} alt="" />
-            </div>
-            <div className="advert-image-box mb-5">
-              <Image src={banner2} alt="" />
-            </div>
-            <div className="advert-image-box mb-5">
-              <Image src={banner3} alt="" />
-            </div>
-            <div className="advert-image-box mb-5">
-              <Image src={banner4} alt="" priority={true} />
-            </div>
-
-            <div className="adverts-box mb-3">
-              Place your Banner Adverts here
+              <div className="adverts-box mb-3">
+                Place your Banner Adverts here
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 };
 
