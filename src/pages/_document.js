26b70aeaa0a1d44f397445c72f1c94ea6bc605d9
Ayodejioladimeji <layobright11@gmail.com>
2023-02-19 08:39:05 +0100

    removed icons

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -10,7 +10,7 @@ export default function Document() {
           sizes="180x180"
           href="/apple-touch-icon.png"
         />
-        <link
+        {/* <link
           rel="icon"
           type="image/png"
           sizes="32x32"
@@ -21,7 +21,7 @@ export default function Document() {
           type="image/png"
           sizes="16x16"
           href="/favicon-16x16.png"
-        />
+        /> */}
 
         <link
           href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
