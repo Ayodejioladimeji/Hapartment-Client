a23e998b59f3ffab6701fe9afbce71b52b40c7d9
layobright <layobright11.com>
2023-03-05 09:40:57 -0800

    added major updates

--- src/components/search.js
+++ src/components/search.js
@@ -199,9 +199,9 @@ const Search = () => {
                   </select>
 
                   <input
-                    // data-aos="fade-up"
-                    // data-aos-once="true"
-                    // data-aos-delay="1400"
+                    data-aos="fade-up"
+                    data-aos-once="true"
+                    data-aos-delay="1400"
                     type="text"
                     onChange={(e) =>
                       setMinPrice(strictAddComma(e.target.value))
@@ -211,9 +211,9 @@ const Search = () => {
                   />
 
                   <input
-                    // data-aos="fade-up"
-                    // data-aos-once="true"
-                    // data-aos-delay="1600"
+                    data-aos="fade-up"
+                    data-aos-once="true"
+                    data-aos-delay="1600"
                     type="text"
                     onChange={(e) =>
                       setMaxPrice(strictAddComma(e.target.value))
