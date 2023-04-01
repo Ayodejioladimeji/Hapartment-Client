fccd686fc77610277f3331d2bc49cdb6778b8a75
layobright <layobright11.com>
2023-04-01 11:14:05 -0700

    removed files

--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-  siteUrl: "https://www.hapartment.org",
-  generateRobotsTxt: true, // (optional)
-  outDir: "./out",
-  // ...other options
-};
