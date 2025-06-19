aff81d0255222c89cca648982fe4c6ab02672f7a
Ayodejioladimeji <layobright11@gmail.com>
2025-06-19 17:03:46 +0100

    Fixed build errors

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,10 +1,7 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
-import { useContext, useState } from "react";
+import { useState } from "react";
 import heroImg from "../../public/images/hero-image.svg";
-import { ACTIONS } from "@/store/Actions";
-import { DataContext } from "@/store/GlobalState";
-import * as gtag from "../lib/gtag";
 
 //
 
