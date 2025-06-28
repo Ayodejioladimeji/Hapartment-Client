ba09584af170f6d4e510cc3662abbc3d4752025e
Ayodejioladimeji <layobright11@gmail.com>
2025-06-28 04:31:31 +0100

    Image fixes

--- src/pages/privacy/index.tsx
+++ src/pages/privacy/index.tsx
@@ -1,5 +1,5 @@
 import BreadCumb from "@/common/breadcumb";
-import privacyImage from "../../public/images/privacy.svg";
+import privacyImage from "/public/images/privacy.svg";
 import Head from "next/head";
 
 const Privacy = () => {
