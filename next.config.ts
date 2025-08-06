a6b19c39ad9e5e54f04c8c399f376e72fb7ee4e6
Ayodejioladimeji <layobright11@gmail.com>
2025-08-06 06:09:02 +0100

    Removed npm

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["res.cloudinary.com", "images.nigeriapropertycentre.com"],
+    // unoptimized: true,
+  },
+  // experimental: {
+  //   scrollRestoration: true,
+  // },
+  // useFileSystemPublicRoutes: false,
+  // distDir: "build",
+  // trailingSlash: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
